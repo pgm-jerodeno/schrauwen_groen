6e2757ccf7947daebc01f1563706200347887b9f
Jeroen Denoyette <114018199+pgm-jerodeno@users.noreply.github.com>
2023-02-16 20:26:42 +0100

    Add files via upload

---
+++ data/highlights.js
@@ -0,0 +1,52 @@
+const HIGHLIGHTS = [
+  {
+    id: "c477ffe1-53ab-4edd-b598-8ae0eb155287",
+    image: "inThePicture_1.jpg",
+    description: "Non ut quia magnam",
+  },
+  {
+    id: "9c12c2ab-afc2-4251-96c1-052058fba544",
+    image: "inThePicture_2.jpg",
+    description: "Nobis qui aliquam ea sequi natus.",
+  },
+  {
+    id: "b825dbcf-0f52-4e43-8703-01d357b3906d",
+    image: "inThePicture_3.jpg",
+    description: "Quia debitis hic quis quos accusamus",
+  },
+  {
+    id: "b950809c-59be-46a1-9ec8-529f7a2b80a3",
+    image: "inThePicture_4.jpg",
+    description: "Iste quasi omnis omnis ipsa.",
+  },
+  {
+    id: "d7946cb9-0964-4e56-ad21-2ec0e11ec064",
+    image: "inThePicture_5.jpg",
+    description: "Voluptatem eos dicta enim reiciendis",
+  },
+  {
+    id: "771f17ee-a794-4d92-8afa-076daf1cc6e0",
+    image: "inThePicture_6.jpg",
+    description: "Rem suscipit aut dicta placeat",
+  },
+  {
+    id: "dc256b22-a2bf-48d5-9ead-64905952b48d",
+    image: "inThePicture_7.jpg",
+    description: "Quis rerum iure. Quo est et. Iste omnis",
+  },
+  {
+    id: "11daba33-0ec8-4de7-8164-79cefe21d595",
+    image: "inThePicture_8.jpg",
+    description: "Ut iusto dolores dolore. Sint voluptatem",
+  },
+  {
+    id: "a64e595d-ef3c-4ac0-859a-0fa973d439fd",
+    image: "inThePicture_9.jpg",
+    description: "Tenetur voluptatem error at",
+  },
+  {
+    id: "ae264e8e-e7ca-42fd-9544-0a0561059596",
+    image: "inThePicture_10.jpg",
+    description: "Error quod qui laboriosam",
+  },  
+];
---
+++ data/services.js
@@ -0,0 +1,58 @@
+const SERVICES = [
+  {
+    id: 'e908f392-7e59-4105-b692-45dbb7a5d69c',
+    type: 'aanleg',
+    picture: 'static/img/aanleg1.jpeg',
+    title: 'Aanleg terras en oprit',
+    synopsis: 'Naast onze specialisatie in het aanleggen en onderhouden van groen, hebben we ook enkele bouwvakkers in ons team. Zij zijn van jongs af aan geschoold in de stiel en specificeren zich op het aanleggen van terrassen, opritten en andere verhardingen.'
+  },
+  {
+    id: '3b1c810c-3db8-493d-8d61-c0c2c157ddd3',
+    type: 'aanleg',
+    picture: 'static/img/aanleg2.jpeg',
+    title: 'Aanplanten bomen, borders en heesters',
+    synopsis: 'We helpen u graag met de keuze van de juiste aanplanten voor uw tuin. Afhankelijk van uw smaak en hoeveel tijd u kunt vrijmaken voor onderthoud, stellen we een mooi aanbod aan bomen, heesteres en borders samen.'
+  },
+  {
+    id: 'd3ef5039-4c0a-4616-955f-1f9f7540926e',
+    type: 'aanleg',
+    picture: 'static/img/aanleg3.jpeg',
+    title: 'Grondwerken',
+    synopsis: 'Een goede grond bormt de fundering van uw tuin. Letterlijk, want enkel een gezonde grond kan beplant, betegeld of bewerkt worden. In veel gevallen is grondbewerking een must om aanplanten gezond te laten groeien.'
+  },
+  {
+    id: '677754bb-99ff-41cd-adeb-582b19e85fca',
+    type: 'aanleg',
+    picture: 'static/img/aanleg4.jpeg',
+    title: 'Aanleg van een gezonde grasmat',
+    synopsis: 'Wilt u graag meteen genieten van de voordelen van gras of ziet u liever een dagelijkse evolutie van zaad tot gras? Afhankelijke van uw wensen, kiezen we voor de beste oplossing. Zo merkt u snel dat het gras aan de overkant toch niet groener is.'
+  },
+  {
+    id: 'c925c3d7-582a-4b67-bd07-e384064e7a42',
+    type: 'onderhoud',
+    picture: 'static/img/onderhoud1.jpg',
+    title: 'Scheren & snoeien',
+    synopsis: 'We scheren & snoeien hagen, fruitbomen, sierbomen & heesters. Daarnaast doen we ook vormsnoei van Buxus, Taxus, enzovoorts'
+  },
+  {
+    id: 'ea068af0-bb71-4ccf-ae06-ab92b48f4303',
+    type: 'onderhoud',
+    picture: 'static/img/onderhoud2.jpg',
+    title: 'Maaien van gazons',
+    synopsis: 'We helpen u graag met de keuze van de juiste aanplanten voor uw tuin. Afhankelijk van uw smaak en hoeveel tijd u kunt vrijmaken voor onderthoud, stellen we een mooi aanbod aan bomen, heesteres en borders samen.'
+  },
+  {
+    id: '454417dd-376b-4961-9e81-427fc2d47807',
+    type: 'onderhoud',
+    picture: 'static/img/onderhoud3.jpg',
+    title: 'Vellen van bomen',
+    synopsis: 'Een goede grond bormt de fundering van uw tuin. Letterlijk, want enkel een gezonde grond kan beplant, betegeld of bewerkt worden. In veel gevallen is grondbewerking een must om aanplanten gezond te laten groeien.'
+  },
+  {
+    id: '7479f8f5-b32c-41f1-8e91-dcd54ff41f81',
+    type: 'onderhoud',
+    picture: 'static/img/onderhoud4.jpg',
+    title: 'Onkruidbestrijding',
+    synopsis: 'Wilt u graag meteen genieten van de voordelen van gras of ziet u liever een dagelijkse evolutie van zaad tot gras? Afhankelijke van uw wensen, kiezen we voor de beste oplossing. Zo merkt u snel dat het gras aan de overkant toch niet groener is.'
+  },
+]
